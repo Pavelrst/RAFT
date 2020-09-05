342f567733cd629961abc0d1bdd796ab347aefd0
Pavelrst <rwcutout@gmail.com>
2020-09-05 22:32:52 +0300

    Add dataset refactoring and Neptune

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, division
 import sys
 sys.path.append('core')
+import numpy as np
 
 import argparse
 import os
@@ -13,29 +14,26 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+import neptune
 
 from torch.utils.data import DataLoader
 from raft import RAFT
 from evaluate import validate_sintel, validate_kitti
 import datasets
-
-# exclude extremly large displacements
-MAX_FLOW = 1000
-SUM_FREQ = 100
-VAL_FREQ = 5000
-
+from args import raft_sintel_ft_args, raft_sintel_debug_args
+from tqdm import tqdm
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
-def sequence_loss(flow_preds, flow_gt, valid):
+def sequence_loss(flow_preds, flow_gt, valid, args):
     """ Loss function defined over sequence of flow predictions """
 
     n_predictions = len(flow_preds)    
     flow_loss = 0.0
 
     # exlude invalid pixels and extremely large diplacements
-    valid = (valid >= 0.5) & (flow_gt.abs().sum(dim=1) < MAX_FLOW)
+    valid = (valid >= 0.5) & (flow_gt.abs().sum(dim=1) < args.max_flow)
 
     for i in range(n_predictions):
         i_weight = 0.8**(n_predictions - i - 1)
@@ -59,22 +57,26 @@ def fetch_dataloader(args):
     """ Create the data loader for the corresponding trainign set """
 
     if args.dataset == 'chairs':
-        train_dataset = datasets.FlyingChairs(args, image_size=args.image_size)
+        train_dataset = datasets.FlyingChairs(args, root=args.dataset_root, image_size=args.image_size)
     
     elif args.dataset == 'things':
-        clean_dataset = datasets.SceneFlow(args, image_size=args.image_size, dstype='frames_cleanpass')
-        final_dataset = datasets.SceneFlow(args, image_size=args.image_size, dstype='frames_finalpass')
+        clean_dataset = datasets.SceneFlow(args, root=args.dataset_root, image_size=args.image_size, dstype='frames_cleanpass')
+        final_dataset = datasets.SceneFlow(args, root=args.dataset_root, image_size=args.image_size, dstype='frames_finalpass')
         train_dataset = clean_dataset + final_dataset
 
     elif args.dataset == 'sintel':
-        clean_dataset = datasets.MpiSintel(args, image_size=args.image_size, dstype='clean')
-        final_dataset = datasets.MpiSintel(args, image_size=args.image_size, dstype='final')
+        clean_dataset = datasets.MpiSintel_Train(args, root=args.dataset_root, image_size=args.image_size, dstype='clean')
+        final_dataset = datasets.MpiSintel_Train(args, root=args.dataset_root, image_size=args.image_size, dstype='final')
+        assert len(clean_dataset) == 908 and len(final_dataset) == 908
         train_dataset = clean_dataset + final_dataset
 
     elif args.dataset == 'kitti':
-        train_dataset = datasets.KITTI(args, image_size=args.image_size, is_val=False)
+        train_dataset = datasets.KITTI(args, root=args.dataset_root, image_size=args.image_size, is_val=False)
 
-    gpuargs = {'num_workers': 4, 'drop_last' : True}
+    else:
+        raise NotImplementedError
+
+    gpuargs = {'num_workers': args.num_of_workers, 'drop_last' : True}
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, 
         pin_memory=True, shuffle=True, **gpuargs)
 
@@ -83,7 +85,7 @@ def fetch_dataloader(args):
 
 def fetch_optimizer(args, model):
     """ Create the optimizer and learning rate scheduler """
-    optimizer = optim.AdamW(model.parameters(), lr=args.lr, weight_decay=args.wdecay, eps=args.epsilon)
+    optimizer = optim.AdamW(model.parameters(), lr=args.lr, weight_decay=args.weight_decay, eps=args.epsilon)
 
     scheduler = optim.lr_scheduler.OneCycleLR(optimizer, args.lr, args.num_steps,
         pct_start=0.2, cycle_momentum=False, anneal_strategy='linear', final_div_factor=1.0)
@@ -92,20 +94,30 @@ def fetch_optimizer(args, model):
     
 
 class Logger:
-    def __init__(self, model, scheduler):
+    def __init__(self, model, scheduler, sum_freq):
         self.model = model
         self.scheduler = scheduler
         self.total_steps = 0
         self.running_loss = {}
+        self.sum_freq = sum_freq
 
     def _print_training_status(self):
-        metrics_data = [self.running_loss[k]/SUM_FREQ for k in sorted(self.running_loss.keys())]
-        training_str = "[{:6d}, {:10.7f}] ".format(self.total_steps+1, self.scheduler.get_lr()[0])
-        metrics_str = ("{:10.4f}, "*len(metrics_data)).format(*metrics_data)
-        
+        metrics_str = ''
+        for key in sorted(self.running_loss.keys()):
+            metric_val = (self.running_loss[key]/self.sum_freq)
+            metrics_str += key+':'+str(np.round(metric_val, 2))+'  '
+            # log to neptune
+            neptune.log_metric(key, metric_val)
+        training_str = "[total steps:{:6d}, lr:{:10.7f}] ".format(self.total_steps+1, self.scheduler.get_lr()[0])
+        neptune.log_metric('learning rate', self.scheduler.get_lr()[0])
+
         # print the training status
         print(training_str + metrics_str)
 
+
+
+
+        # zero running loss
         for key in self.running_loss:
             self.running_loss[key] = 0.0
 
@@ -118,13 +130,12 @@ class Logger:
 
             self.running_loss[key] += metrics[key]
 
-        if self.total_steps % SUM_FREQ == SUM_FREQ-1:
+        if self.total_steps % self.sum_freq == self.sum_freq-1:
             self._print_training_status()
             self.running_loss = {}
 
 
 def train(args):
-
     model = RAFT(args)
     model = nn.DataParallel(model)
     print("Parameter Count: %d" % count_parameters(model))
@@ -142,7 +153,7 @@ def train(args):
     optimizer, scheduler = fetch_optimizer(args, model)
 
     total_steps = 0
-    logger = Logger(model, scheduler)
+    logger = Logger(model, scheduler, args.sum_freq)
 
     should_keep_training = True
     while should_keep_training:
@@ -153,7 +164,7 @@ def train(args):
             optimizer.zero_grad()
             flow_predictions = model(image1, image2, iters=args.iters)
             
-            loss, metrics = sequence_loss(flow_predictions, flow, valid)
+            loss, metrics = sequence_loss(flow_predictions, flow, valid, args)
             loss.backward()
 
             torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
@@ -163,49 +174,32 @@ def train(args):
 
             logger.push(metrics)
 
-            if total_steps % VAL_FREQ == VAL_FREQ-1:
-                PATH = 'checkpoints/%d_%s.pth' % (total_steps+1, args.name)
-                torch.save(model.state_dict(), PATH)
+            if total_steps % args.val_freq == args.val_freq-1:
+                path = 'checkpoints/%d_%s.pth' % (total_steps+1, args.experiment_name)
+                torch.save(model.state_dict(), path)
 
             if total_steps == args.num_steps:
                 should_keep_training = False
                 break
 
+    path = 'checkpoints/%s.pth' % args.experiment_name
+    torch.save(model.state_dict(), path)
 
-    PATH = 'checkpoints/%s.pth' % args.name
-    torch.save(model.state_dict(), PATH)
-
-    return PATH
+    return path
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--name', default='bla', help="name your experiment")
-    parser.add_argument('--dataset', help="which dataset to use for training") 
-    parser.add_argument('--restore_ckpt', help="restore checkpoint")
-    parser.add_argument('--small', action='store_true', help='use small model')
-
-    parser.add_argument('--lr', type=float, default=0.00002)
-    parser.add_argument('--num_steps', type=int, default=100000)
-    parser.add_argument('--batch_size', type=int, default=6)
-    parser.add_argument('--image_size', type=int, nargs='+', default=[384, 512])
-
-    parser.add_argument('--iters', type=int, default=12)
-    parser.add_argument('--wdecay', type=float, default=.00005)
-    parser.add_argument('--epsilon', type=float, default=1e-8)
-    parser.add_argument('--clip', type=float, default=1.0)
-    parser.add_argument('--dropout', type=float, default=0.0)
-    args = parser.parse_args()
-
-    torch.manual_seed(1234)
-    np.random.seed(1234)
-
     if not os.path.isdir('checkpoints'):
         os.mkdir('checkpoints')
 
+    neptune.init('pavelrasto/RAFT-Scopeflow')
+    neptune.create_experiment(name='Scopeflow augmentor 1', description='Scopeflow augmentorr without photometric transformations')
+
+    args = raft_sintel_ft_args(exp_name='sintel_ft_sflo_aug_no_photometric', augment_type='scopeflow_augmentor')
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
     # scale learning rate and batch size by number of GPUs
     num_gpus = torch.cuda.device_count()
     args.batch_size = args.batch_size * num_gpus
     args.lr = args.lr * num_gpus
-
     train(args)
