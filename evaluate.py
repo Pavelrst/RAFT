485ab6e8fb5149a9880fbc2aa9a06a35c6b58010
Pavelrst <rwcutout@gmail.com>
2020-09-17 19:48:12 +0300

    Evaluation script modified, more convinient

--- evaluate.py
+++ evaluate.py
@@ -23,6 +23,8 @@ def validate_sintel(args, model, iters=50):
     model.eval()
     pad = 2
 
+    result_dict = {'epe_clean': -1, 'epe_final': -1}
+
     for dstype in ['clean', 'final']:
         val_dataset = datasets.MpiSintel_Val(args, root=args.dataset_root, do_augument=False, dstype=dstype)
         assert len(val_dataset) > 0
@@ -44,6 +46,14 @@ def validate_sintel(args, model, iters=50):
             epe_list.append(epe.item())
 
         print("Validation (%s) EPE: %f" % (dstype, np.mean(epe_list)))
+        if dstype == 'clean':
+            result_dict['epe_clean'] = np.mean(epe_list)
+        elif dstype == 'final':
+            result_dict['epe_final'] = np.mean(epe_list)
+        else:
+            raise NotImplementedError
+
+    return  result_dict
 
 
 def validate_kitti(args, model, iters=32):
@@ -82,17 +92,28 @@ def validate_kitti(args, model, iters=32):
     print("Validation KITTI: %f, %f" % (np.mean(epe_list), 100*np.mean(out_list)))
 
 
+
 if __name__ == '__main__':
-    CKPT_PATH = '.\\checkpoints'
+    '''
+    This function validates all skpts in all subdirectories of CKPT_PATH
+    '''
+    PATH = '../RAFT_golden_ckpts'
+
     raft_ckpts = []
-    sflo_ckpts = []
-    for ckpt_path in os.listdir(CKPT_PATH):
-        if 'random_crop_v3' in ckpt_path:
-            raft_ckpts.append(os.path.join(CKPT_PATH, ckpt_path))
+    for subdir in os.listdir(PATH):
+        subdir_path = os.path.join(PATH, subdir)
+        for ckpt_name in os.listdir(subdir_path):
+            if '.pth' in ckpt_name:
+                raft_ckpts.append(os.path.join(subdir_path, ckpt_name))
+
+    print('Validate on following ckpt:')
+    for ckpt_name in raft_ckpts:
+        print(ckpt_name)
+
+    res_dict = dict(zip(raft_ckpts, np.zeros(len(raft_ckpts))))
 
     # Validate RAFT augmentor per steps
-    for ckpt in raft_ckpts:
-        # args = raft_sintel_val_args('.\\checkpoints\\sintel_ft_raft_aug.pth')
+    for ckpt in res_dict.keys():
         print('============ validate', ckpt)
         args = raft_sintel_val_args(ckpt)
 
@@ -103,4 +124,43 @@ if __name__ == '__main__':
         model.to('cuda')
         model.eval()
 
-        validate_sintel(args, model, args.iters)
+        res_dict[ckpt] = validate_sintel(args, model, args.iters)
+
+    # build plot
+    labels = [os.path.basename(x) for x in res_dict.keys()]
+    epe_clean = [np.round(res_dict[key]['epe_clean'], 2) for key in res_dict.keys()]
+    epe_final = [np.round(res_dict[key]['epe_final'], 2) for key in res_dict.keys()]
+
+    x = np.arange(len(labels))  # the label locations
+    width = 0.35  # the width of the bars
+
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width / 2, epe_clean, width, label='epe_clean')
+    rects2 = ax.bar(x + width / 2, epe_final, width, label='epe_final')
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('EPE')
+    ax.set_title('EPE comparison, -b2 -iter240K')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend()
+
+
+    def autolabel(rects):
+        """Attach a text label above each bar in *rects*, displaying its height."""
+        for rect in rects:
+            height = rect.get_height()
+            ax.annotate('{}'.format(height),
+                        xy=(rect.get_x() + rect.get_width() / 2, height),
+                        xytext=(0, 3),  # 3 points vertical offset
+                        textcoords="offset points",
+                        ha='center', va='bottom')
+
+
+    autolabel(rects1)
+    autolabel(rects2)
+
+    fig.tight_layout()
+
+    plt.tight_layout()
+    plt.savefig('foo.png')
