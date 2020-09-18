57664d104324f22cdf7d8fabded1653c427b0f48
Pavelrst <rwcutout@gmail.com>
2020-09-19 00:59:39 +0300

    reflect pad fixed

--- core/utils/reflect_pad.py
+++ core/utils/reflect_pad.py
@@ -8,16 +8,16 @@ def reflect_pad(img):
     ch_num = img.shape[2]
 
     TOP_PAD = 0
-    DOWN_PAD = np.round(img[0]*0.5)
-    LEFT_PAD = np.round(img[1]*0.25)
-    RIGHT_PAD = np.round(img[1]*0.25)
+    DOWN_PAD = int(img.shape[0]*0.5)
+    LEFT_PAD = int(img.shape[1]*0.25)
+    RIGHT_PAD = int(img.shape[1]*0.25)
 
     res = np.zeros((img.shape[0]+TOP_PAD+DOWN_PAD, img.shape[1]+LEFT_PAD+RIGHT_PAD, img.shape[2]))
 
     for ch in range(ch_num):
         res[:, :, ch] = np.pad(img[:, :, ch], ((TOP_PAD, DOWN_PAD), (LEFT_PAD, RIGHT_PAD)), 'reflect')
 
-    return res
+    return res.astype(np.uint8)
 
 
 
